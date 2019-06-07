eaf42d3aff5c0163197a2020c6a07097ec9241da
theirix <theirix@gmail.com>
2019-06-07 10:03:11 +0300

    Periodic task

--- app/celery.py
+++ app/celery.py
@@ -15,6 +15,7 @@ def setup_periodic_tasks(sender, **kwargs):
     print('Setup tasks')
     import app.tasks
     sender.add_periodic_task(60.0, app.tasks.check_timelapse_task.s(), name='check_timelapse_task')
+    sender.add_periodic_task(300.0, app.tasks.archive_task.s(), name='archive_task')
 
 
 if __name__ == '__main__':
